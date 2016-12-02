d436c6fc38d37c6d4a123d611181b8940ce03880
Tim_Yen <Pega@PegatekiMacBook-Pro.local>
2016-12-02 15:43:56 -0800

    Add result to file

--- crawler.js
+++ crawler.js
@@ -1,6 +1,6 @@
 var request = require("request");
 var cheerio = require("cheerio");
-
+var fs = require("fs");
 
 request({
 	url: "http://www.businessweekly.com.tw/",
@@ -11,8 +11,14 @@ request({
 	}
 	var $ = cheerio.load(response);
 	var result = [];
-	var titles = $("a");
-	for (var i = 0; i < titles.length; i++) {
-		console.log($(titles[i]).text() + "  " + $(titles[i]).attr('href'));
+	var hyperlink = $("a");
+	for (var i = 0; i < hyperlink.length; i++) {
+		console.log($(hyperlink[i]).text() + "  " + $(hyperlink[i]).attr('href'));
+		var json = {
+			text: $(hyperlink[i]).text(),
+			url: $(hyperlink[i]).attr('href')
+		};
+		result.push(json);
 	}
+	fs.writeFileSync("crawler_result.json", JSON.stringify(result));
 });
