fda7788aca8f686872b0f9ca70ac4426b7fa8558
Tim_Yen <Pega@PegatekiMacBook-Pro.local>
2016-12-02 18:38:50 -0800

    Random parse URL
    Add list limit
    Auto remove old url

--- crawler.js
+++ crawler.js
@@ -2,38 +2,77 @@ var request = require("request");
 var cheerio = require("cheerio");
 var fs = require("fs");
 
+var dataListLimit = 300;
+var oneTimeParseLimit = 50;
+var overLoadSplitSize = 100;
 
 function getParseData(url, data, fn) {
+	cleanList(data);
+
 	request({
-		url: "http://www.businessweekly.com.tw/",
+		url: url,
 		method: "GET"
 	}, function(error, rquest, response) {
 		if (error || !response) {
+			console.log("Error");
 			return;
 		}
 		var $ = cheerio.load(response);
 		var list = [];
 		var hyperlink = $("a");
-		var parseSize = hyperlink.length >= 30 ? 30 : hyperlink.length;
-		for (var i = 0; i < parseSize; i++) {
-			console.log($(hyperlink[i]).text() + "  " + $(hyperlink[i]).attr('href'));
-			var text = $(hyperlink[i]).text();
-			var url = $(hyperlink[i]).attr('href');
-			if (!url || url.indexOf("http") !== 0 || text.indexOf(" ") === 0) {
+		console.log("hyperlink : " + hyperlink.length);
+		var count = 0;
+		for (var i = 0; i < hyperlink.length; i++) {
+			// console.log($(hyperlink[i]).text() + "  " + $(hyperlink[i]).attr('href'));
+			if (!canAddToList($(hyperlink[i]), data)) {
 				continue;
 			}
 			var json = {
-				text: text,
-				url: url
+				text: $(hyperlink[i]).text(),
+				url: $(hyperlink[i]).attr('href')
 			};
 			list.push(json);
+			count++;
+			if (count >= oneTimeParseLimit) {
+				break;
+			}
 		}
 
 		var result = data.concat(list);
 
 		writeFile(result);
 	});
+
+	function cleanList(data) {
+		console.log("data size : " + data.length);
+		if (data.length > dataListLimit) {
+			data.splice(0, overLoadSplitSize);
+		}
+	}
+
+	function canAddToList(hyperlinkObject, data) {
+		var text = hyperlinkObject.text();
+		var url = hyperlinkObject.attr('href');
+
+		if (!text) {
+			return false;
+		}
+
+		if (!url || url.indexOf("http") !== 0) {
+			return false;
+		}
+
+		for (var i = data.length - 1; i >= 0; i--) {
+			var linkObj = data[i];
+			if (hyperlinkObject.attr('href') === linkObj["url"]) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
+
+
 
 function writeFile(data) {
 	fs.writeFileSync("crawler_result.json", JSON.stringify(data));
@@ -46,7 +85,8 @@ function readFile(fn) {
 		}
 
 		var jsonArray = JSON.parse(data)
-		var url = "http://www.businessweekly.com.tw/";
+			// var url = "http://www.businessweekly.com.tw/";
+		var url = "https://tw.yahoo.com/";
 		if (jsonArray.length !== 0) {
 			var randomNumber = Math.floor((Math.random() * jsonArray.length) + 1);
 			url = jsonArray[randomNumber - 1]["url"];
@@ -54,7 +94,7 @@ function readFile(fn) {
 
 		console.log(url);
 
-		fn("url", JSON.parse(data));
+		fn(url, JSON.parse(data));
 	});
 }
 
