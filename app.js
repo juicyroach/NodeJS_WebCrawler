f8b4b2733bb9142191508d371e01e79bba1338ca
Tim_Yen <Pega@PegatekiMacBook-Pro.local>
2016-12-13 11:38:05 -0800

    Test EJS

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+	res.render('display', {
+		title: 'Test EJS',
+		users: ['Juicyroach', 'Irene']
+	})
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(3000, function() {
+	console.log('Example app listening on port 3000!');
 });
