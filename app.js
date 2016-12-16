18023656c9a5c11726e0023cb2cea569dd9bb1ba
Tim_Yen <Pega@PegatekiMacBook-Pro.local>
2016-12-15 18:11:26 -0800

    Add port setting for Heroku Also add nodejs version setting

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var crawler = require("./crawler");
 
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/views'));
 
@@ -131,9 +133,10 @@ app.get('/', function(req, res) {
 	}
 });
 
-app.listen(3000, function() {
-	console.log('Example app listening on port 3000!');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
+
 
 
 var schedule = require('node-schedule');
